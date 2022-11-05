90f6f89e67398c8daf9eb32067198d7c939d7575
dsonyah <doceliasonyah@gmail.com>
2022-11-05 04:42:11 -0400

    new_config

--- js/config.js
+++ js/config.js
@@ -3,9 +3,9 @@ window._config = {
         userPoolId: '', // us-east-1_L058uipnC
         userPoolClientId: '', // 5p8rhnlonnoh80ck5v9p1qq04h
 
-        region: '' // e.g. us-east-1
+        region: '' //  us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' //  https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
     }
 };
