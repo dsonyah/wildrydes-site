e1d87671d81fb2452ab08d86f147927680ef13f1
dsonyah <doceliasonyah@gmail.com>
2022-11-07 13:13:10 -0500

    new config

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 window._config = {
     cognito: {
-        userPoolId: '', // us-east-1_L058uipnC
-        userPoolClientId: '', // 5p8rhnlonnoh80ck5v9p1qq04h
+        userPoolId: 'us-east-1_L058uipnC', // us-east-1_L058uipnC
+        userPoolClientId: '5p8rhnlonnoh80ck5v9p1qq04h', // 5p8rhnlonnoh80ck5v9p1qq04h
 
-        region: '' //  us-east-1
+        region: ' us-east-1' //  us-east-1
     },
     api: {
         invokeUrl: '' //  https://lbdwf6jb88.execute-api.us-east-1.amazonaws.com/dev',
