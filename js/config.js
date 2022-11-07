d1217556f46832ec4d3459c6feeff360500e4ff1
dsonyah <doceliasonyah@gmail.com>
2022-11-07 11:27:53 -0500

    new config

--- js/config.js
+++ js/config.js
@@ -6,6 +6,6 @@ window._config = {
         region: '' //  us-east-1
     },
     api: {
-        invokeUrl: '' //  https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
+        invokeUrl: '' //  https://lbdwf6jb88.execute-api.us-east-1.amazonaws.com/my-api',
     }
 };
